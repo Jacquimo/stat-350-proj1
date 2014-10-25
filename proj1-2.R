cd9988c30bc8d97b0c819b2c031a3150dd896c1f
Gray Houston <ghousto@purdue.edu>
2014-10-25 04:16:31 -0400

    First commit

---
+++ proj1-2.R
@@ -0,0 +1,45 @@
+####### Proj. 1 - Quest. 1 #######
+library(OIdata)
+library(ggplot2)
+library(gridExtra)
+
+mu <- 0.5
+sd <- sqrt(1.0 / 12.0)
+meanData <- c()
+sdData <- c()
+sample_n <- c(1, 2, 9, 16)
+
+for (n in sample_n) {  
+  xbar <- 0.0
+  mean_dist <- c() # all of the mean values for each sampling
+  
+  # calculate the sampling distribution of means and some other things
+  for (i in 1:1000) {
+    dist <- runif(n, max=1, min=0) # make the uniform distribution values
+    mean <- mean(dist)
+    mean_dist <- c(mean_dist, mean)
+    xbar <- xbar + mean
+  }
+  xbar <- xbar / 1000.0 # calculate the average of the 1000 runs
+  meanData <- c(meanData, xbar)
+  sdData <- c(sdData, sd(mean_dist))
+
+  # Make the graphs
+  hist(mean_dist, xlab="Sample xbar", ylab="Frequency", 
+       main=paste("Sample Mean Uniform Distribution for N=", n), las=1, xlim=c(0, 1))
+  qqnorm(mean_dist, main=paste("Sample Mean Uniform Quantile Plot for N=", n), las=1)
+  qqline(mean_dist)
+}
+
+# make table
+mean_dist_table <- matrix(c(sample_n, meanData, rep(mu, length(sample_n)), sdData, 
+                            rep(sd, length(sample_n))), ncol=5)
+colnames(mean_dist_table) <- c("n", "mean of 1000 xbar's", "theoretical mean", 
+                               "std. dev. of 1000 xbar's", "theoretical std dev.")
+mean_dist_table <- as.table(mean_dist_table)#, col.names=colnames)
+
+qplot(1:10, 1:10, geom="blank") +
+theme_bw() + theme(line=element_blank(), text=element_blank()) +
+  annotation_custom(grob=tableGrob(mean_dist_table, gpar.coltext=gpar(cex=1.2), 
+                                   gpar.rowtext=gpar(cex=1.2)), xmin=-Inf, xmax=Inf, 
+                                    ymin=-Inf, ymax=Inf)
